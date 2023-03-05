697a356903517442b8ce84bc9b5238065b44e349
aricin <aricin.rotarius@gmail.com>
2023-03-05 14:16:36 -0600

    Added users

--- main.js
+++ main.js
@@ -1,47 +1,89 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue, push, child, onChildAdded, onChildChanged } from "firebase/database";
+import { getDatabase, ref, set, push, child, onChildAdded, onChildChanged, onChildRemoved, onDisconnect, onValue, remove } from "firebase/database";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Insert firebaseConfig here:
-
+/* ----- Firebase Stuff ----- */
+const firebaseConfig = {
+    apiKey: "AIzaSyDWsXAWQW9u4dJKVVqW1EN-1fB8CSB0LbQ",
+    authDomain: "apple-crisp.firebaseapp.com",
+    databaseURL: "https://apple-crisp-default-rtdb.firebaseio.com",
+    projectId: "apple-crisp",
+    storageBucket: "apple-crisp.appspot.com",
+    messagingSenderId: "541844444487",
+    appId: "1:541844444487:web:12f3fb46688a463c10e862"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 
-// Values used for constructing a deck of cards
-const suits = ["Clubs", "Diamond", "Hearts", "Spades"];
-const values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+/* ----- Lobby Stuff ----- */
+let lobbyID;
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get("lobby")) {
+    lobbyID = urlParams.get("lobby");
+} else {
+    lobbyID = push(child(ref(db), 'lobbies/')).key;
+    //let lobby = db.collection('lobby');
+    window.location.href = 'http://127.0.0.1:5173/?lobby=' + lobbyID;
+}
 
 
-let gameContainer;  // Entire game board including opponents
-let lakeContainer;  // Community piles
-let yourStockPile;       // Your main deck that you draw from
-let yourWastePile;   // Your face up pile that you draw cards to
-let yourWorkPiles;
-let yourWorkPile1;  // The piles that you can move cards around in
-let yourWorkPile2;  //
-let yourWorkPile3;
-let yourWorkPile4;
-let yourWorkPile5;  // 
-let yourObjectivePile;  // The objective cards (if you empty it the game is over)
-let draggingCard;   // The card that is currently being dragged by the user
+/* ----- User Stuff----- */
+let userID;
+let userRef;
+const auth = getAuth();
 
-let isDragging;     // True if a card is currently being dragged
-let startMouseX;    // X value of mouse when clicked
-let startMouseY;    // Y value of mouse when clicked
-let startPile;      // The origin pile of the card currently being dragged
-let pileCount;      // Number of piles in community board
-let validPlacement; // True if the user is dragging a card over a pile where it is allowed to be dropped
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    userID = user.uid;
+    userRef = ref(db, `lobbies/${lobbyID}/users/${userID}`);
+
+    set(userRef, {name: 'player'});
+    onDisconnect(userRef).remove();
+  } else {
+    // User signed out
+  }
+});
+
+signInAnonymously(auth)
+    .then(() => {
+    // Signed in..
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+});
+
+// Duh, this won't work because when the last user closes the app there isn't an instance of the code anymore
+onValue(ref(db, `lobbies/${lobbyID}/users/`), (snapshot) => {
+    if(!(snapshot.exists())) {
+        console.log("hello sir");
+        //remove(ref(db, `lobbies/${lobbyID}/lake`));
+        set(ref(db, `lobbies/${lobbyID}`), null);
+    }
+});
+
+/* ----- User Preferences ----- */
+let nameForm = document.getElementById("name-form");
+nameForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    nameField.blur();
+});
+
+let nameField = document.getElementById("name-field")
+nameField.addEventListener('input', function(e) {
+    e.preventDefault();
+    set(userRef, {name: nameField.value})
+});
 
 
-const NUM_BLITZ_CARDS = 10;
-const NUM_PLAY_PILES = 3; // TODO: Make this dependant on the number of players
+/* ----- Supporting game functions ----- */
 
 /**
  * Constructs an in order deck of cards into an array, 
@@ -49,6 +91,8 @@ const NUM_PLAY_PILES = 3; // TODO: Make this dependant on the number of players
  * @returns {Array} deck
  */
 function buildDeck() {
+    let suits = ["Clubs", "Diamond", "Hearts", "Spades"];
+    let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
     let deck = [];
 
     for (let suit of suits) {
@@ -84,8 +128,8 @@ function shuffleDeck(deck) {
 
 /**
  * Returns the path to the given card image in a string
- * @param  {Card} card Card object containing a 'suit' and 'value
- * @return {String} cardImg String correspsonding to the given card img path
+ * @param  {Card} card
+ * @return {String} cardImg
  */
 function get_card_img(card) {
     let cardImg = `url('/card-pngs/${card.suit} ${card.value}.png')`
@@ -94,9 +138,10 @@ function get_card_img(card) {
 }
 
 /**
- * Adds the given card to the specified pile
- * @param  {Card} card Card object containing a 'suit' and 'value
- * @param  {Pile} pile
+ * Updates the array and background image of the pile element.
+ * Confirms that the pile is set to active.
+ * @param  {Card} card
+ * @param  {HTMLElement} pile
  */
 function add_card_to_pile(card, pile) {
     if (pile.cards.length == 0) {
@@ -108,9 +153,9 @@ function add_card_to_pile(card, pile) {
 
 /**
  * Removes the top card from the given pile. Updates the array and
- * background image of the pile's div.
- * @param  {Pile} pile Pile object representing the face-up piles on
- * the gameboard. Contains a 'cards' array and a 'div' element.
+ * background image of the pile. Sets pile.active to false if there
+ * are not more cards in the pile.
+ * @param  {HTMLElement} pile 
  */
 function remove_card_from_pile(pile) {
     let poppedCard = pile.cards.pop();
@@ -129,15 +174,23 @@ function remove_card_from_pile(pile) {
 }
 
 
-/* ---- Dragging Stuff ---- */
+/* ---- Dragging Functions ---- */
 
+/**
+ * Checks of the given pile is a valid spot to place the card currently being dragged.
+ * @param {HTMLElement} pile 
+ * @returns {boolean}
+ */
 function is_valid_placement(pile) {
-
+    // The dragged card is over the "lake"...
     if (pile.classList.contains("community-cards")) {
+        // The card is over a valid pile
         if (pile.cards[pile.cards.length-1].suit == draggingCard.cards[0].suit && pile.cards[pile.cards.length-1].value == draggingCard.cards[0].value - 1) {
             return true;
-        }    
+        }
+    // The card is over the "work pikes"...
     } else if (pile.classList.contains("work-piles")) {
+        // The card is over a valid pile
         if (pile.cards[pile.cards.length-1].suit == draggingCard.cards[0].suit && pile.cards[pile.cards.length-1].value - 1 == draggingCard.cards[0].value) {
             return true;
         }    
@@ -146,8 +199,13 @@ function is_valid_placement(pile) {
     }
 }
 
-// The function run when the user clicks on the gameboard
-let mouse_down = function(e) {
+/** 
+ * Runs when the user clicks on a pile that this function is applied to:
+ *  - waste pile
+ *  - active work piles
+ *  - objective pile
+ */
+let grab_card = function(e) {
     e.preventDefault();
 
     // If pile contains a movable card
@@ -166,7 +224,11 @@ let mouse_down = function(e) {
     }
 }
 
-let mouse_move = function(e) {
+/** 
+ * Runs when the user drags a card.
+ * Updates the position of the card being dragged relative to the users cursor.
+ */
+let drag_card = function(e) {
     if (!isDragging) {
         return;
     } else {
@@ -187,24 +249,11 @@ let mouse_move = function(e) {
     }
 }
 
-function update_lake(card, id) {
-    console.log(card);
-    if (card.value == 1) {
-        let newPile = document.createElement("div");
-        newPile.classList.add("your-cards");
-        newPile.classList.add("community-cards");
-        newPile.id = id;
-        newPile.style.backgroundImage = get_card_img(card);
-        newPile.cards = [card];
-        lakeContainer.appendChild(newPile);
-    } else {
-        let pile = document.getElementById(id);
-        pile.style.backgroundImage = get_card_img(card);
-    }
-}
-
-
-let mouse_up = function(e) {
+/** 
+ * Runs when the user drags a card.
+ * Updates the position of the card being dragged relative to the users cursor.
+ */
+let place_card = function(e) {
     if (!isDragging) {
         return
     } else {
@@ -216,14 +265,14 @@ let mouse_up = function(e) {
 
         if (targetElements.includes(lakeContainer)) {
             if (draggingCard.cards[0].value == 1) { // If card is an Ace
-                let newPileId = push(child(ref(db), 'lake')).key;
-                set(ref(db, `lake/${newPileId}`), {
+                let newPileId = push(child(ref(db), `lobbies/${lobbyID}/lake`)).key;
+                set(ref(db, `lobbies/${lobbyID}/lake/${newPileId}`), {
                     card: draggingCard.cards[0]
                 });
             } else if (is_valid_placement(targetElements[1])) {
                 console.log(targetElements[1]);
                 let id = targetElements[1].id;
-                set(ref(db, `lake/${id}`), {
+                set(ref(db, `lobbies/${lobbyID}/lake/${id}`), {
                     card: draggingCard.cards[0]
                 });
             } else {
@@ -254,6 +303,9 @@ let mouse_up = function(e) {
     }
 }
 
+
+/* ---- Stock Pile Functions ---- */
+
 let draw_card = function(e) {
     e.preventDefault();
 
@@ -274,53 +326,94 @@ let draw_card = function(e) {
     }
 }
 
-let reset_animation = function(e) {
+let reset_animation = function() {
     yourWastePile.classList.remove("draw-animation");
     yourStockPile.classList.remove("reset-deck");
 }
 
-let reset_deck = function(e) {
-    console.log("hi");
-    if (yourStockPile.cards.length == 0) {
-        yourStockPile.cards = yourWastePile.cards.reverse();
-        yourWastePile.cards = [];
+/* ----- Initialize the game ----- */
 
-        yourStockPile.style.backgroundImage = "url('/card-pngs/Back\ Red\ 2.png')";
-        yourWastePile.style.backgroundImage = null;
+// Game elements
+let gameContainer = document.getElementById("game-container");   // Where all the game elements are contained
+let lakeContainer = document.getElementById("lake-container");   // Community piles - each player can stack cards onto them
+let yourStockPile = document.getElementById("your-stock-pile");  // Your main deck that you draw from
+let yourWastePile = document.getElementById("your-waste-pile");  // Your face up pile that you draw cards to
+let yourWorkPiles = document.getElementById("your-work-piles");  // The piles that you can move cards around in
+let yourWorkPile1 = document.getElementById("your-work-pile-1");
+let yourWorkPile2 = document.getElementById("your-work-pile-2");
+let yourWorkPile3 = document.getElementById("your-work-pile-3");
+let yourWorkPile4 = document.getElementById("your-work-pile-4"); 
+let yourWorkPile5 = document.getElementById("your-work-pile-5"); 
+let yourObjectivePile = document.getElementById("your-objective-pile"); // The objective cards (if you empty it the game is over)
 
-        yourWastePile.active = false;
+yourStockPile.onclick = draw_card;
+yourStockPile.onmouseup = reset_animation;
+
+yourWastePile.onmousedown = grab_card;
+yourWorkPile1.onmousedown = grab_card;
+yourWorkPile2.onmousedown = grab_card;
+yourWorkPile3.onmousedown = grab_card;
+yourWorkPile4.onmousedown = grab_card;
+yourWorkPile5.onmousedown = grab_card;
+yourObjectivePile.onmousedown = grab_card;
+
+gameContainer.onmousemove = drag_card;
+gameContainer.onmouseup = place_card;
+
+// Initialize dragging card
+let draggingCard = document.createElement("div"); // The card that is currently being dragged by the user
+draggingCard.cards = [];
+draggingCard.classList.add("your-cards");
+draggingCard.id = "dragging-card";
+draggingCard.draggable = true;
+
+let isDragging = false; // True if a card is currently being dragged
+let startMouseX;        // X value of mouse when initially clicked
+let startMouseY;        // Y value of mouse when initially clicked
+let startPile;          // The origin pile of the card currently being dragged
+
+// Game variables
+const NUM_BLITZ_CARDS = 10;
+const NUM_WORK_PILES = 3; // TODO: Make this dependant on the number of players
+
+
+/* ----- Lake Listeners ----- */
+
+let lakeRef = ref(db, `lobbies/${lobbyID}/lake/`);
+onChildAdded(lakeRef, (snapshot) => {
+    if (snapshot.exists()) {
+        let card = snapshot.val().card;
+        let id = snapshot.key;
+        let newPile = document.createElement("div");
+        newPile.classList.add("your-cards");
+        newPile.classList.add("community-cards");
+        newPile.id = id;
+        newPile.style.backgroundImage = get_card_img(card);
+        newPile.cards = [card];
+        lakeContainer.appendChild(newPile);
     }
-}
+});
+onChildChanged(lakeRef, (snapshot) => {
+    if (snapshot.exists()) {
+        let card = snapshot.val().card;
+        let id = snapshot.key;
+        let pile = document.getElementById(id);
+        pile.cards.push(card);
+        pile.style.backgroundImage = get_card_img(card);
+    }
+});
+onChildRemoved(lakeRef, (snapshot) => {
+    if (snapshot.exists()) {
+        lakeContainer.removeChild(document.getElementById(snapshot.key));
+    }
+});
 
-/* ---- Community Stuff ---- */
-let mouse_over = function(e) {
-    //e.preventDefault();
-    validPlacement = true;
-    console.log(e.target);
-}
 
-let mouse_leave = function(e) {
-    //e.preventDefault();
-    validPlacement = false;
-    console.log(validPlacement);
-}
+/* ----- Buttons ----- */
 
-
-function new_game() {
+let dealCards = document.getElementById("deal-cards");
+dealCards.addEventListener("click", () => {
     let deck = shuffleDeck(buildDeck());
-
-    // Initialize game elements
-    gameContainer = document.getElementById("game-container");
-    lakeContainer = document.getElementById("lake-container");
-    yourStockPile = document.getElementById("your-stock-pile");
-    yourWastePile = document.getElementById("your-waste-pile");
-    yourWorkPiles = document.getElementById("your-work-piles");
-    yourWorkPile1 = document.getElementById("your-work-pile-1");
-    yourWorkPile2 = document.getElementById("your-work-pile-2");
-    yourWorkPile3 = document.getElementById("your-work-pile-3");
-    yourWorkPile4 = document.getElementById("your-work-pile-4"); 
-    yourWorkPile5 = document.getElementById("your-work-pile-5"); 
-    yourObjectivePile = document.getElementById("your-objective-pile");
 
     // Initialize each piles cards array
     yourStockPile.cards = deck;
@@ -333,6 +426,7 @@ function new_game() {
         pile.active = true;
     }
     yourWastePile.active = false;
+    yourWastePile.style.backgroundImage = null;
 
     // Deal out the initial game state
     add_card_to_pile(remove_card_from_pile(yourStockPile), yourWorkPile1);
@@ -345,41 +439,15 @@ function new_game() {
     }
 
     yourStockPile.style.backgroundImage = "url('/card-pngs/Back\ Red\ 2.png')";
+});
 
-    // Initialize dragging card
-    draggingCard = document.createElement("div");
-    draggingCard.cards = [];
-    draggingCard.classList.add("your-cards");
-    draggingCard.id = "dragging-card";
-    draggingCard.draggable = true;
-    isDragging = false;
+let clearLake = document.getElementById("clear-lake");
+clearLake.addEventListener("click", () => {
+    set(ref(db, `lobbies/${lobbyID}/lake/`), null);
+});
 
-    // When db updates
-    set(ref(db), null);
-    let lakeRef = ref(db, 'lake/');
-    onChildAdded(lakeRef, (snapshot) => {
-        if (snapshot.exists()) {
-            update_lake(snapshot.val().card, snapshot.key);
-        }
-    });
-    onChildChanged(lakeRef, (snapshot) => {
-        if (snapshot.exists()) {
-            update_lake(snapshot.val().card, snapshot.key);
-        }
-    });
-}
-new_game();
+let invite = document.getElementById("invite");
+invite.addEventListener("click", () => {
+    navigator.clipboard.writeText('http://127.0.0.1:5173/?lobby=' + lobbyID);
+});
 
-yourStockPile.onclick = draw_card;
-yourStockPile.onmouseup = reset_animation;
-
-yourWastePile.onmousedown = mouse_down
-yourWorkPile1.onmousedown = mouse_down;
-yourWorkPile2.onmousedown = mouse_down;
-yourWorkPile3.onmousedown = mouse_down;
-yourWorkPile4.onmousedown = mouse_down;
-yourWorkPile5.onmousedown = mouse_down;
-yourObjectivePile.onmousedown = mouse_down;
-
-gameContainer.onmousemove = mouse_move;
-gameContainer.onmouseup = mouse_up;
